c59d7b7d4dd40cc8f494ba602b25531401e4e713
andrea-10 <74046679+andrea-10@users.noreply.github.com>
2020-11-07 12:33:45 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace macchinine
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pct1 = new System.Windows.Forms.PictureBox();
+            this.pct2 = new System.Windows.Forms.PictureBox();
+            this.pct3 = new System.Windows.Forms.PictureBox();
+            this.bgw1 = new System.ComponentModel.BackgroundWorker();
+            this.bgw2 = new System.ComponentModel.BackgroundWorker();
+            this.btn1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pct1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pct1
+            // 
+            this.pct1.Image = global::macchinine.Properties.Resources.macchinabianca;
+            this.pct1.Location = new System.Drawing.Point(46, 72);
+            this.pct1.Name = "pct1";
+            this.pct1.Size = new System.Drawing.Size(291, 142);
+            this.pct1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct1.TabIndex = 2;
+            this.pct1.TabStop = false;
+            this.pct1.Click += new System.EventHandler(this.pct1_Click);
+            // 
+            // pct2
+            // 
+            this.pct2.Image = global::macchinine.Properties.Resources.macchinarossa;
+            this.pct2.Location = new System.Drawing.Point(46, 346);
+            this.pct2.Name = "pct2";
+            this.pct2.Size = new System.Drawing.Size(291, 144);
+            this.pct2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct2.TabIndex = 1;
+            this.pct2.TabStop = false;
+            // 
+            // pct3
+            // 
+            this.pct3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pct3.Image = global::macchinine.Properties.Resources.road_trip_background_10864211;
+            this.pct3.Location = new System.Drawing.Point(0, 0);
+            this.pct3.Name = "pct3";
+            this.pct3.Size = new System.Drawing.Size(1804, 568);
+            this.pct3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct3.TabIndex = 0;
+            this.pct3.TabStop = false;
+            this.pct3.Click += new System.EventHandler(this.pct3_Click);
+            // 
+            // bgw1
+            // 
+            this.bgw1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw1_DoWork);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(46, 254);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(203, 65);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "avvia";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1804, 568);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.pct1);
+            this.Controls.Add(this.pct2);
+            this.Controls.Add(this.pct3);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pct1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pct3;
+        private System.Windows.Forms.PictureBox pct2;
+        private System.Windows.Forms.PictureBox pct1;
+        private System.ComponentModel.BackgroundWorker bgw1;
+        private System.ComponentModel.BackgroundWorker bgw2;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace macchinine
+
+    //programma che simuli la corsa tra 2 macchine
+{
+    public partial class FrmMain : Form
+    {
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+
+            //inizio
+            bgw1.DoWork += bgw1_DoWork;
+            bgw2.DoWork += bgw1_DoWork;
+
+
+            //quando finisce, vanno nello stessa fine
+            bgw1.RunWorkerCompleted += bgw1_RunWorkerCompleted;
+
+
+            bgw2.RunWorkerCompleted += bgw1_RunWorkerCompleted;
+
+
+
+
+        }
+
+
+        private void bgw1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //torna visibile il bottone avvia
+            btn1.Visible = true;
+        }
+
+        private void pct3_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        //procedimento gara
+        private void bgw1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Random r = new Random();
+            
+            int numero = 0;
+            int numero2 = 0;
+
+
+          //prende la posizione delle picture box
+            int posy = pct1.Location.Y;
+            int posy2 = pct2.Location.Y;
+
+
+            //ciclo for che permette di generare ogni volta dei numeri random, 1000 è un numero scelto a piacere
+            for (int i = 0; i < 1000; i++)
+            {
+                numero += r.Next(0, 30);
+                numero2 += r.Next(0, 30);
+                pct1.Location = new Point(numero, posy);
+                pct2.Location = new Point(numero2, posy2);
+                Thread.Sleep(50);
+
+
+
+                //arriva al traguardo
+                if (numero > 1500)
+                {
+                    DialogResult drg = MessageBox.Show(" riprovare?", "LA MACCHINA NERA HA VINTO!", MessageBoxButtons.YesNo);
+
+
+
+
+                    if (drg == DialogResult.No)
+                    {
+                        Application.Exit();
+                    }
+
+
+                    bgw1.CancelAsync();
+                    bgw2.CancelAsync();
+
+                }
+
+
+
+
+                //arriva al traguardo
+                if (numero2 > 1500)
+                {
+                    DialogResult drg = MessageBox.Show(" riprovare?", "LA MACCHINA ROSSA HA VINTO!", MessageBoxButtons.YesNo);
+
+
+                    if (drg == DialogResult.No)
+                    {
+                        Application.Exit();
+                    }
+
+                    bgw1.CancelAsync();
+                    bgw2.CancelAsync();
+
+                }
+            }
+
+        }
+
+        private void pct1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //parte la gara
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+
+            bgw1.RunWorkerAsync();
+            bgw2.RunWorkerAsync();
+
+
+            btn1.Visible = false;
+
+
+        }
+    }
+}
